f4abc72aeead8313e90b7f9433b291cdfe3d1090
Maheesha <maheeshasithum@gmail.com>
2021-12-25 12:18:20 +0530

    C# brackets

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::io;
 
-fn main() {
-    println!("Please enter the text you want to encrypt or decrypt with ROT13 
-if you enter text thats already encrypted with ROT13 it will get decrypted");
+fn main()
+{
+    println!(r#"Please enter the text you want to encrypt or decrypt with ROT13 if you enter text thats already encrypted with ROT13 it will get decrypted"#);
     let mut string = String::new();
     io::stdin().read_line(&mut string).expect("Failed to read line");
     let char_vec: Vec<char> = string.chars().collect();
@@ -10,16 +10,20 @@ if you enter text thats already encrypted with ROT13 it will get decrypted");
     println!("{}",the_string);
 }
 
-fn encrypt(cvec: Vec<char>)-> String{
+fn encrypt(cvec: Vec<char>)-> String
+{
     let mut v: Vec<char> = Vec::new();
     for &i in &cvec{
-        if i.is_ascii_alphabetic() && !i.is_ascii_whitespace(){
+        if i.is_ascii_alphabetic() && !i.is_ascii_whitespace()
+        {
             let i_as_ascii = i as u8;
             let ascii_comparison: u8 = if i.is_ascii_lowercase() {109} else {77};
-            if i_as_ascii <= ascii_comparison{
+            if i_as_ascii <= ascii_comparison
+            {
                 let ascii_to_char: char = (i_as_ascii + 13) as char;
                  v.push(ascii_to_char);
-            }else { 
+            }else 
+            { 
                 let ascii_to_char: char = (i_as_ascii - 13) as char;
                 v.push(ascii_to_char);
             }
